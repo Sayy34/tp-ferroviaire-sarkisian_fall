438c9363fc4087acdc42838dda47e6b1b904a217
Enzo SARKISSIAN <sarkissian2004@gmail.com>
2026-02-01 18:08:07 +0100

    Création cartographie-lib et Gare.java

---
+++ src/main/java/fr/cours/info/tp/ferroviaire/models/Gare.java
@@ -0,0 +1,39 @@
+package fr.cours.info.tp.ferroviaire.models;
+
+public class Gare {
+    private String nom;
+    private int nombreQuais;
+
+    // Constructeurs
+    public Gare() {}
+
+    public Gare(String nom, int nombreQuais) {
+        this.nom = nom;
+        this.nombreQuais = nombreQuais;
+    }
+
+    // Getters & Setters
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public int getNombreQuais() {
+        return nombreQuais;
+    }
+
+    public void setNombreQuais(int nombreQuais) {
+        this.nombreQuais = nombreQuais;
+    }
+
+    @Override
+    public String toString() {
+        return "Gare{" +
+                "nom='" + nom + '\'' +
+                ", nombreQuais=" + nombreQuais +
+                '}';
+    }
+}
